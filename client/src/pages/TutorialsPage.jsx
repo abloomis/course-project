ff76769c198190d39c0fda57a061155b73e21851
alex loomis <loomisa@oregonstate.edu>
2025-05-05 23:19:57 -0700

    Created and embedded tutorial videos for TutorialsPage.jsx

--- client/src/pages/TutorialsPage.jsx
+++ client/src/pages/TutorialsPage.jsx
@@ -1,14 +1,60 @@
+import React from 'react';
 import {Link} from "react-router-dom";
 
-function TutorialsPage() {
+// use <iframe> to easily embed youtube videos
+// right-clicking on the yt video and picking the embed option will provide html code
+function TutorialPage() {
   return (
-    <div className="faq-page">
+    <div>
       <h1>Tutorials</h1>
 
+      <p>Watch the videos below for a walkthrough of the site's features.</p>
+
+      <h2>1. Import</h2>
+      <iframe 
+        width="670" 
+        height="377" 
+        src="https://www.youtube.com/embed/QH00Qkvj6Hw" 
+        title="Ranking Tool Tutorial 1 - Importing a File" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen
+      ></iframe>
+
+      <h2>2. Edit</h2>
+      <iframe 
+        width="670" 
+        height="377" 
+        src="https://www.youtube.com/embed/C2iN-Oeleds" 
+        title="Ranking Tool Tutorial 2 - Editing a Table" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen
+      ></iframe>
+
+      <h2>3. Export</h2>
+      <iframe 
+        width="670" 
+        height="377" 
+        src="https://www.youtube.com/embed/9WNI9yE1tqA" 
+        title="Ranking Tool Tutorial 3 - Exporting to a File" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen
+      ></iframe>
+
+      
+
+      <div></div>
 
       <Link to="/">Return</Link>
+
     </div>
   );
+  
 }
 
-export default TutorialsPage;
+export default TutorialPage;
